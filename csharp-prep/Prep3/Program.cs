c88b1fabd03d71dd22d2e0e790eaa14b45be2229
Matt <medangina@gmail.com>
2025-09-20 14:06:42 -0600

    Prep 3 program, stretch 2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -6,35 +6,43 @@ class Program
     {
         //Declare variables
         int guess = -1;   //Set to -1 at start so won't match user number
-        int guesses = 0;
+        int guesses = 0;   
+        string response = "yes";   //Default to yes so runs loop
 
-        //Get magic number from random generator
-        Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1, 101);
-
-        //Use do-while loop to get first guess before checking
-        do
+        while (response == "yes")
         {
-            //Get guess number from user and convert
-            Console.Write("What is you guess? ");
-            string userGuess = Console.ReadLine();
-            guess = int.Parse(userGuess);
+            //Get magic number from random generator
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 101);
 
-            //Add one to guess counter
-            guesses++;
-
-            //Check if guess is higher or lower than magic number
-            if (guess < number)
+            //Use do-while loop to get first guess before checking
+            do
             {
-                Console.WriteLine("Higher");
-            }
-            else if (guess > number)
-            {
-                Console.WriteLine("Lower");
-            }
-        } while (guess != number);
+                //Get guess number from user and convert
+                Console.Write("What is you guess? ");
+                string userGuess = Console.ReadLine();
+                guess = int.Parse(userGuess);
 
-        //Loop will exit then print statement
-        Console.WriteLine($"You guessed it! This took {guesses} tries.");
+                //Add one to guess counter
+                guesses++;
+
+                //Check if guess is higher or lower than magic number
+                if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+            } while (guess != number);
+
+            //Loop will exit then print statement
+            Console.WriteLine($"You guessed it! This took {guesses} tries.");
+            
+            //Ask if they want to play again and loop back to beginning
+            Console.Write("Would you like to play again? ");
+            response = Console.ReadLine();
+        }
     }
 }
