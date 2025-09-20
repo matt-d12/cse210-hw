611555429055fae3bda8783fe453016cab44fdfb
Matt <medangina@gmail.com>
2025-09-20 10:48:37 -0600

    Prep 2 program, req 3

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,34 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
+        string letterGrade = "";
+
         Console.Write("What is your grade percentage? ");
         string userInput = Console.ReadLine();
         int grade = int.Parse(userInput);
 
         if (grade >= 90)
         {
-            Console.WriteLine("You got an A.");
+            letterGrade = "A";
         }
-
         else if (grade >= 80)
         {
-            Console.WriteLine("You got a B.");
+            letterGrade = "B";
         }
-
         else if (grade >= 70)
         {
-            Console.WriteLine("You got a C.");
+            letterGrade = "C";
         }
-
         else if (grade >= 60)
         {
-            Console.WriteLine("You got a D.");
+            letterGrade = "D";
         }
-
         else if (grade < 60)
         {
-            Console.WriteLine("You got a F.");
+            letterGrade = "F";
         }
+
+        Console.WriteLine($"Your grade is a {letterGrade}.");
 
         if (grade >= 70)
         {
