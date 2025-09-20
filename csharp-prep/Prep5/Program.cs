254ff7d97cdcafa75036c03532834da2b37806a1
Matt <medangina@gmail.com>
2025-09-20 17:19:22 -0600

    Prep 5 program, req 3&4

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -15,18 +15,39 @@ class Program
         name = Console.ReadLine();
     }
 
+    //Function to ask for user's favorite number
+    static void PromptUserNumber(ref int number)
+    {
+        Console.Write("Please enter your favorite number: ");
+        string textNumber = Console.ReadLine();
+        number = int.Parse(textNumber);
+    }
+
+    //Function to ask user's birth year
+    static void PromptUserBirthYear(out int year)
+    {
+        Console.Write("Please enter the year you were born: ");
+        string textYear = Console.ReadLine();
+        year = int.Parse(textYear);
+
+    }
 
     static void Main(string[] args)
     {
         //Establish variables
         string name = "";
+        int number = 0;
+        int year = 0;
         
         //Call functions
         DisplayWelcome();
         PromptUserName(ref name);
+        PromptUserNumber(ref number);
+        PromptUserBirthYear(out year);
 
         //Outputs
-        Console.WriteLine($"{name}, the square of your number is");
+        Console.WriteLine($"{name}, your favorite number is {number}");
+        Console.WriteLine($"{name}, you were born in {year}");
 
     }
 }
