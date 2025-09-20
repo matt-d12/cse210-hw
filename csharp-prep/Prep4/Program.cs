444ea24ed3d2d223359384c05bbf8a3c882f396b
Matt <medangina@gmail.com>
2025-09-20 14:50:10 -0600

    Prep 4 program, req 1

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //Create initial list & declare variable(s)
+        List<int> numbers = new List<int>();
+        int number = -1;
+        int sum = 0;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        while (number != 0)
+        {
+            //Get numbers from user and convert
+            Console.Write("Enter number: ");
+            string textNumber = Console.ReadLine();
+            number = int.Parse(textNumber);
+            
+            //Add number to list and to sum
+            numbers.Add(number);
+            sum = sum + number;
+        } 
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        /* //Loop to check list is working as intended
+        for (int i= 0; i < numbers.Count; i++)
+        {
+            Console.WriteLine(numbers[i]);
+        }
+        */
     }
 }
