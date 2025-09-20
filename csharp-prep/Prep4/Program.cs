9fa3899086630302107719ce8a65db04977ae9c7
Matt <medangina@gmail.com>
2025-09-20 14:57:30 -0600

    Prep 4 program, req 2

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Create initial list & declare variable(s)
+        //Create initial list & declare variables
         List<int> numbers = new List<int>();
         int number = -1;
         int sum = 0;
@@ -18,12 +18,20 @@ class Program
             string textNumber = Console.ReadLine();
             number = int.Parse(textNumber);
             
-            //Add number to list and to sum
-            numbers.Add(number);
-            sum = sum + number;
+            //Add number to list and to sum unless 0 to avoid count issues for average
+            if (number != 0)
+            {
+                numbers.Add(number);
+                sum = sum + number;
+            }
         } 
 
+        //Calculate average
+        float average = (float)sum / numbers.Count;
+
+        //Display results
         Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
 
         /* //Loop to check list is working as intended
         for (int i= 0; i < numbers.Count; i++)
