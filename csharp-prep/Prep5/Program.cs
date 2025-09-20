8a7db79b9ef17989bc1ff62bff6ed4a2028b6e5d
Matt <medangina@gmail.com>
2025-09-20 17:02:41 -0600

    Prep 5 program, req 1&2

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,31 @@ using System;
 
 class Program
 {
+    //Function to display welcome message
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    //Function to ask for user's name
+    static void PromptUserName(ref string name)
+    {
+        Console.Write("Please enter your name: ");
+        name = Console.ReadLine();
+    }
+
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        //Establish variables
+        string name = "";
+        
+        //Call functions
+        DisplayWelcome();
+        PromptUserName(ref name);
+
+        //Outputs
+        Console.WriteLine($"{name}, the square of your number is");
+
     }
 }
