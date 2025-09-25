adbf49694c5a81e9dff04cb3cd72d9fc576975cc
Matt <medangina@gmail.com>
2025-09-24 19:38:17 -0600

    Unit 02 Prep, step 5

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -6,4 +6,9 @@ public class Job
     public int _startYear;
     public int _endYear;
 
+    //Display formatting
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
 }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -6,12 +6,20 @@ class Program
     {
         //Create new job instance
         Job job1 = new Job();
-        Job job2 = new Job();
         job1._jobTitle = "Software Engineer";
-        job2._jobTitle = "IT Tech";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        //Second job instance
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
 
         //Output
-        Console.WriteLine(job1._jobTitle);
-        Console.WriteLine(job2._jobTitle);
+        job1.Display();
+        job2.Display();
     }
 }
