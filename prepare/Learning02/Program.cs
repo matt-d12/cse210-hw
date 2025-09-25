d20a6b35ea2738d5e0a9533b5cd2860fab5fe65c
Matt <medangina@gmail.com>
2025-09-24 19:31:34 -0600

    Unit 02 Prep, step 4

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //Create new job instance
+        Job job1 = new Job();
+        Job job2 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job2._jobTitle = "IT Tech";
+
+        //Output
+        Console.WriteLine(job1._jobTitle);
+        Console.WriteLine(job2._jobTitle);
     }
 }
