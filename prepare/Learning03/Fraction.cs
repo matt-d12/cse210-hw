c13bd789539bd66ca999564c0d6e19ad7482471d
Matt <medangina@gmail.com>
2025-09-30 13:56:48 -0600

    Unit 03 Prep, step 4

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -5,4 +5,29 @@ public class Fraction
     //Establish attributes
     private int _top;
     private int _bottom;
+
+    //Construct default fraction
+    public Fraction()
+    {
+        //Default with no parameters should be set to 1/1
+        _top = 1;
+        _bottom = 1;
+    }
+
+    //Construct for single parameter
+    public Fraction(int wholeNumber)
+    {
+        //Pass in whole number for numerator
+        _top = wholeNumber;
+        //Denomiator set to 1 as whole number
+        _bottom = 1;
+    }
+
+    //Construct when passing in two parameters
+    public Fraction(int top, int bottom)
+    {
+        //Pass in and set new variables
+        _top = top;
+        _bottom = bottom;
+    }
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        //Initialize new fractions
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1);
+
+        Fraction f2 = new Fraction(6);
+        Console.WriteLine(f2);
+
+        Fraction f3 = new Fraction(6, 7);
+        Console.WriteLine(f3);
     }
 }
