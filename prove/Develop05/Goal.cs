f09ce7d833ba51e0e8fcce3b1e65cc9614b2bb27
Matt <medangina@gmail.com>
2025-10-18 08:08:26 -0700

    Unit 05 Dev, parent class and menu

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,54 @@
+using System;
+
+public abstract class Goal
+{
+    //Establish attributes
+    private string _name = "";
+    private string _description = "";
+    private int _points = 0;
+
+    //Constructor method
+    public Goal(string name, string description, int points)
+    {
+        _name = name;
+        _description = description;
+        _points = points;
+    }
+
+    //Getter for name
+    protected string GetName()
+    {
+        return _name;
+    }
+
+    //Getter for description
+    public string GetDescription()
+    {
+        return _description;
+    }
+
+    //Getter for points
+    protected int GetPoints()
+    {
+        return _points;
+    }
+
+    //Setter for name
+    protected void SetName(string name)
+    {
+        _name = name;
+    }
+
+    //Setter for description
+    protected void SetDescription(string description)
+    {
+        _description = description;
+    }
+    
+    //Setter for points
+    protected void SetPoints(int points)
+    {
+        _points = points;
+    }
+
+}
--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,67 @@
 using System;
 
+//ADD EXCEEDING CORE REQUIREMENTS HERE
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        //Establish attribute for choice
+        string choice = "";
+
+        //Loop for user to control menu
+        while (choice != "6")
+        {
+            //Show the menu method below that returns user input and set it to choice attribute
+            choice = DisplayMenu();
+
+            if (choice == "1")                //Create new goal
+            {
+                //TBD
+            }
+            else if (choice == "2")           //List goals
+            {
+                //TBD
+            }
+            else if (choice == "3")           //Save goals
+            {
+                //TBD
+            }
+            else if (choice == "4")           //Load goals
+            {
+                //TBD
+            }
+            else if (choice == "5")           //Record an event
+            {
+                //TBD
+            }
+            else if (choice == "6")
+            {
+                //Quit program
+            }
+            else
+            {
+                //Invalid choice please try again
+            }
+        }
+    }
+
+    //Method for the diplay menu
+    static string DisplayMenu()
+    {
+        Console.Clear();
+        //Show current points - REPLACE WITH COUNTER LATER
+        Console.WriteLine("You have 0 points");
+        Console.WriteLine();
+        //Show menu options
+        Console.WriteLine("  1. Create New Goal");
+        Console.WriteLine("  2. List Goals");
+        Console.WriteLine("  3. Save Goals");
+        Console.WriteLine("  4. Load Goals");
+        Console.WriteLine("  5. Record Event");
+        Console.WriteLine("  6. Quit");
+        //Get user input and do the following
+        Console.Write("Select a choice from the menu: ");
+        return Console.ReadLine();
     }
 }
