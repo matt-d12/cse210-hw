293eb18260e1c910d6dbf5d84ac9a423579fde5e
Matt <medangina@gmail.com>
2025-10-25 20:58:55 -0600

    test order

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        //Customer 1
+        Address address1 = new Address("123 Main St", "Rexburg", "ID", "USA");
+        Customer customer1 = new Customer("John Smith", address1);
+        Order order1 = new Order(customer1);
+        order1.AddProduct(new Product("Phone", "A1001", 499.99, 1));
+        order1.AddProduct(new Product("Case", "B2001", 49.99, 2));
+
+        //Customer 2
+
+        //Display Order 1
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order1.GetTotalCost():0.00}\n");
+
+        //Display Order 2
+
     }
 }
