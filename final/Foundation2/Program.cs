1e4c11d4c86e155c00f810599d135273abe8cd87
Matt <medangina@gmail.com>
2025-10-25 21:09:59 -0600

    Update Program.cs

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -12,6 +12,11 @@ class Program
         order1.AddProduct(new Product("Case", "B2001", 49.99, 2));
 
         //Customer 2
+        Address address2 = new Address("123 Not Main St", "Toronto", "ON", "Canada");
+        Customer customer2 = new Customer("Sara Smith", address2);
+        Order order2 = new Order(customer2);
+        order2.AddProduct(new Product("Computer", "D3001", 1499.99, 1));
+        order2.AddProduct(new Product("Case", "E4001", 149.99, 2));
 
         //Display Order 1
         Console.WriteLine(order1.GetPackingLabel());
@@ -19,6 +24,8 @@ class Program
         Console.WriteLine($"Total Cost: ${order1.GetTotalCost():0.00}\n");
 
         //Display Order 2
-
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order2.GetTotalCost():0.00}\n");
     }
 }
