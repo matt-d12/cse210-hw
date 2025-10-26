d0add7842ef73f3be949ed3d0f5e57b448fd86e8
Matt <medangina@gmail.com>
2025-10-25 20:37:57 -0600

    Update Order.cs

--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -13,4 +13,41 @@ public class Order
         _customer = customer;
     }
 
+    //Method to add products to list
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    //Method to get total cost
+    public double GetTotalCost()
+    {
+        double total = 0;
+        foreach (Product p in _products)
+        {
+            //Add total cost
+        }
+
+        //Add shipping cost
+
+        return total;
+    }
+
+    //Method to create packing label
+    public string GetPackingLabel()
+    {
+        string label = "Packing Label:";
+        foreach (Product p in _products)
+        {
+            label += " - " + p.GetProductInfo();
+        }
+        return label;
+    }
+
+    //Method to create shipping label
+    public string GetShippingLabel()
+    {
+        //shipping label
+    }
+
 }
