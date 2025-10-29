04187247fec6e3366b573b8568c4d7e5b7de1a76
Matt <medangina@gmail.com>
2025-10-28 21:20:00 -0600

    class creations

---
+++ final/Foundation4/Activity.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Activity
+{
+    //Establish attributes
+
+    //Constructor
+
+    //Method to get distance
+
+    //Method to get speed
+
+    //Method to get pace
+
+    //Method to get summary info
+}
---
+++ final/Foundation4/Swimming.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Swimming : Activity
+{
+    //Establish attributes
+
+    //Constructor
+
+    //Method to override distance
+
+    //Method to override speed
+
+    //Method to override pace
+
+    //Method to override summary info
+}
