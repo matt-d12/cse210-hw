d3a279af845f94d3b815d2a65043edcfeab74712
Matt <medangina@gmail.com>
2025-10-28 21:41:26 -0600

    display creation and testing

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        //Create new list to store activity entries
+        List<Activity> activities = new List<Activity>
+        {
+            new Running("28 Oct 2025", 10, 1.5),
+            new Cycling("27 Oct 2025", 15, 2.0),
+            new Swimming("26 Oct 2025", 20, 5)
+        };
+
+        //Loop through list and display summary
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
