b8be932c16d5214cc75adc905bb8693de7b81982
Matt <medangina@gmail.com>
2025-10-28 21:31:42 -0600

    running activity class

--- final/Foundation4/Running.cs
+++ final/Foundation4/Running.cs
@@ -3,14 +3,34 @@ using System;
 public class Running : Activity
 {
     //Establish attributes
+    private double _distance;
 
     //Constructor
+    public Running(string date, double minutes, double distance)
+        : base(date, minutes)
+    {
+        _distance = distance;
+    }
 
     //Method to override distance
+    public override double GetDistance() => _distance;
 
     //Method to override speed
+    public override double GetSpeed()
+    {
+        return (GetDistance() / Minutes) * 60;
+    }
 
     //Method to override pace
+    public override double GetPace()
+    {
+        return Minutes / GetDistance();
+    }
 
     //Method to override summary info
+    public override string GetSummary()
+    {
+        return $"{Date} Running ({Minutes} min): Distance {GetDistance():0.0} km, " +
+            $"Speed {GetSpeed():0.0} kph, Pace: {GetPace():0.00} min per km";
+    }
 }
