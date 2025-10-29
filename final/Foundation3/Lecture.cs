306d52e4392d777b701916ce84ea0401ee6bbf9c
Matt <medangina@gmail.com>
2025-10-28 20:02:48 -0600

    lecture event creation

--- final/Foundation3/Lecture.cs
+++ final/Foundation3/Lecture.cs
@@ -3,9 +3,21 @@ using System;
 public class Lecture : Event
 {
     //Establish attributes
+    private string _speaker;
+    private int _capacity;
 
     //Constructor
+    public Lecture(string title, string description, string date, string time, Address address, string speaker, int capacity)
+        : base(title, description, date, time, address)
+    {
+        _speaker = speaker;
+        _capacity = capacity;
+    }
 
     //Method to get and override full details
+    public override string GetFullDetails()
+    {
+        return $"{GetStandardDetails()}\nType: Lecture\nSpeaker: {_speaker}\nCapacity: {_capacity}";
+    }
 
 }
