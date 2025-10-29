87cd1457b597be0399eaafe08ed0d62a052d2940
Matt <medangina@gmail.com>
2025-10-28 20:00:20 -0600

    base event class creation

--- final/Foundation3/Event.cs
+++ final/Foundation3/Event.cs
@@ -3,13 +3,38 @@ using System;
 public class Event
 {
     //Establish attributes
+    private string _title;
+    private string _description;
+    private string _date;
+    private string _time;
+    private Address _address;
 
     //Constructor
+    public Event(string title, string description, string date, string time, Address address)
+    {
+        _title = title;
+        _description = description;
+        _date = date;
+        _time = time;
+        _address = address;
+    }
 
-    //Method to get standard details 
+    //Method to get standard details
+    public string GetStandardDetails()
+    {
+        return $"Title: {_title}\nDescription: {_description}\nDate: {_date}nTime: {_time}\nAddress:\n{_address.GetFullAddress()}";
+    }
 
     //Method to get short description
+    public virtual string GetShortDescription()
+    {
+        return "";     //Will be overridden by child classes
+    }
 
     //Method to get full description
+    public virtual string GetFullDetails()
+    {
+        return GetStandardDetails();     //Will be overridden by child classes
+    }
 
 }
