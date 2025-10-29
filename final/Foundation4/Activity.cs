8fc578ddcaaae74a5c8207de8d943f5be0cea74d
Matt <medangina@gmail.com>
2025-10-28 21:26:09 -0600

    base activity class

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -3,14 +3,32 @@ using System;
 public class Activity
 {
     //Establish attributes
+    private string _date;
+    private double _minutes;
 
     //Constructor
+    public Activity(string date, double minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    //Public setters to be read-only
+    public string Date => _date;
+    public double Minutes => _minutes;
 
     //Method to get distance
+    public virtual double GetDistance() => 0;
 
     //Method to get speed
+    public virtual double GetSpeed() => 0;
 
     //Method to get pace
+    public virtual double GetPace() => 0;
 
     //Method to get summary info
+    public virtual string GetSummary()
+    {
+        return $"{_date} Activity ({_minutes} min)";
+    }
 }
