dcb11c1a60b3313f69cf978be53774b302865217
Matt <medangina@gmail.com>
2025-10-28 21:37:20 -0600

    cycling and swimming activity classes

--- final/Foundation4/Cycling.cs
+++ final/Foundation4/Cycling.cs
@@ -3,14 +3,34 @@ using System;
 public class Cycling : Activity
 {
     //Establish attributes
+    private double _speed;
 
     //Constructor
+    public Cycling(string date, double minutes, double speed)
+        : base(date, minutes)
+    {
+        _speed = speed;
+    }
 
     //Method to override distance
+    public override double GetSpeed() => _speed;
 
     //Method to override speed
+    public override double GetDistance()
+    {
+        return (Minutes / 60) * _speed;
+    }
 
     //Method to override pace
+    public override double GetPace()
+    {
+        return 60 / _speed;
+    }
 
     //Method to override summary info
+    public override string GetSummary()
+    {
+        return $"{Date} Cycling ({Minutes} min): Distance {GetDistance():0.0} km, " +
+            $"Speed {GetSpeed():0.0} kph, Pace: {GetPace():0.00} min per km";
+    }
 }
--- final/Foundation4/Swimming.cs
+++ final/Foundation4/Swimming.cs
@@ -3,14 +3,38 @@ using System;
 public class Swimming : Activity
 {
     //Establish attributes
+    private int _laps;
+    private double _lapLength = 50;
 
     //Constructor
+    public Swimming(string date, double minutes, int laps)
+        : base(date, minutes)
+    {
+        _laps = laps;
+    }
 
     //Method to override distance
+    public override double GetDistance()
+    {
+        return _laps * _lapLength / 1000;
+    }
 
     //Method to override speed
+    public override double GetSpeed()
+    {
+        return (GetDistance() / Minutes) * 60;
+    }
 
     //Method to override pace
+    public override double GetPace()
+    {
+        return Minutes / GetDistance();
+    }
 
     //Method to override summary info
+    public override string GetSummary()
+    {
+        return $"{Date} Swimming ({Minutes} min): Distance {GetDistance():0.0} km, " +
+            $"Speed {GetSpeed():0.0} kph, Pace: {GetPace():0.00} min per km";
+    }
 }
